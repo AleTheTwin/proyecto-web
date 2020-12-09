b13bf07958f86a116256abc35eb2859dc17b2f6d
AleTheTwin <alethetwin@icloud.com>
2020-12-09 10:41:42 -0600

    implementación de cookies

---
+++ HTML/assets/dist/js/main.js
@@ -0,0 +1,52 @@
+var sesionCookie = getCookie("Sesion")
+var botonCerrarSesion = document.createElement("a");
+botonCerrarSesion.class = "py-2";
+botonCerrarSesion.href = "#";
+botonCerrarSesion.id = "cerrarSesion";
+botonCerrarSesion.style = "position:relative; top: 8px;"
+if(sesionCookie != "") {
+    //aquí desactivas el botón login y registro en cambio pone botón de usuario
+    botonCerrarSesion.innerHTML = "Cerrar sesion";
+    document.getElementById("seccionTop").appendChild(botonCerrarSesion);
+    document.getElementById("botonLogIn").innerHTML = sesionCookie;
+    document.getElementById("botonLogIn").href = "#";
+    document.getElementById("botonLogIn").innerHTML = sesionCookie;
+    document.getElementById("botonLogIn").href = "#";
+}
+
+
+botonCerrarSesion.addEventListener('click', function () {
+    deleteCookie("Sesion");
+    location.href = "/HTML/Ventanas/Index.html";
+})
+
+function deleteCookie(correo){
+    valor="";
+    expiracion="";
+    var d = new Date();
+    d.setTime(d.getTime()+expiracion*24*60*60*1000);
+    var expira = "expieres="+d.toUTCString();
+    document.cookie = correo+ "=" + valor +";" + expira +";path=/";
+}
+
+function setCookie(correo, valor, expiracion){
+    var d = new Date();
+    d.setTime(d.getTime()+expiracion*24*60*60*1000);
+    var expira = "expieres="+d.toUTCString();
+    document.cookie = correo+ "=" + valor +";" + expira +";path=/";
+}
+
+function getCookie(correo){
+    var nom= correo +"=";
+    var array = document.cookie.split(";");
+    for(var i=0; i<array.length; i++){
+        var c = array[i];
+        while (c.charAt(0)==" "){
+            c= c.substring(1);
+        }
+        if(c.indexOf(correo)==0){
+            return c.substring(correo.length + 1, c.length);
+        }
+    }
+    return  "";
+}
